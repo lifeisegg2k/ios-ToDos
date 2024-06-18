acb5da974ec800db1f6aa762d8403a782be80624
lifeisegg2k <lifeisegg@gmail.com>
2024-06-18 12:42:19 +0900

    view add

---
+++ ToDos/View/ReminderRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ReminderRowView.swift
+//  ToDos
+//
+//  Created by yuchul Kim on 2024/06/18.
+//
+
+import SwiftUI
+
+struct ReminderRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReminderRowView()
+}
