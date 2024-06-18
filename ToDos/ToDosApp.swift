a9584bd016cdf4f7c4d2ba568ef3d116a7e6eb9c
lifeisegg2k <lifeisegg@gmail.com>
2024-06-18 14:06:53 +0900

    done

--- ToDos/ToDosApp.swift
+++ ToDos/ToDosApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct ToDosApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
+        .modelContainer(for: ReminderList.self)
     }
 }
