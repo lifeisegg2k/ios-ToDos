6ef5b94ba4b47d0008255f6b5a5968309e2139d0
lifeisegg2k <lifeisegg@gmail.com>
2024-06-18 12:33:42 +0900

    Initial Commit

---
+++ ToDos/ToDosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDosApp.swift
+//  ToDos
+//
+//  Created by yuchul Kim on 2024/06/18.
+//
+
+import SwiftUI
+
+@main
+struct ToDosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
